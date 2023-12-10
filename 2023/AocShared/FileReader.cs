ccd525cea3422bfc3170d400b6bb7e1b73f8cb2f
Davideee <dimauro.davide@hotmail.ch>
2023-12-11 00:21:15 +0100

    Day10 part 1

--- 2023/AocShared/FileReader.cs
+++ 2023/AocShared/FileReader.cs
@@ -3,19 +3,23 @@
 /// <summary>
 /// Liest ein txt. File ein.
 /// </summary>
-public class FileReader {
+public class FileReader
+{
 
     public string[] Lines;
 
-    private const string FilePath = "./data/data.txt";
+    private const string FilePath = "./data/";
 
+    private char[,] Matrix;
 
-    public FileReader(string path = "") {
-        var readPath = string.IsNullOrEmpty(path) ? FilePath : path;
+    public FileReader(string path = "")
+    {
+        var readPath = string.IsNullOrEmpty(path) ? FilePath + "data.txt" : FilePath + path;
         ReadLines(readPath);
     }
 
-    private void ReadLines(string path) {
+    private void ReadLines(string path)
+    {
         // Pfad zur Datei, die Sie einlesen möchten
         string[] lines = Array.Empty<string>();
 
@@ -29,5 +33,34 @@ public class FileReader {
             // Fehlerbehandlung für den Fall, dass die Datei nicht gelesen werden kann
             Console.WriteLine("Fehler beim Lesen der Datei: " + e.Message);
         }
+    }
+
+    public char[,] GetMatrix()
+    {
+        Matrix = new char[Lines.Length, Lines[0].Length];
+
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            for (int j = 0; j < Lines[i].Length; j++)
+            {
+                Matrix[i, j] = Lines[i][j];
+            }
+        }
+        return Matrix;
+    }
+
+    public Tuple<int, int>? GetKoordinates(char c)
+    {
+        for (int i = 0; i < Matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < Matrix.GetLength(1); j++)
+            {
+                if (Matrix[i, j] == c)
+                {
+                    return Tuple.Create(i, j);
+                }
+            }
+        }
+        return null;
     }
 }
---
+++ 2023/Day10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using AocShared;
+using Day10;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Day09
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            var file = new FileReader("data.txt");
+            char[,] fields = file.GetMatrix();
+            Tuple<int, int> start = file.GetKoordinates('S');
+            PipeNavigator pipeNavigator = new PipeNavigator(fields, start);
+            stopwatch.Stop();
+            Console.WriteLine($"Part {1}: {pipeNavigator.GetFarestCount}");
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
+
+
+
