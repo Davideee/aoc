82f4356d5b0c014de40236a85a7ef5054861c997
Davide <dimauro.gaet@gmail.com>
2024-12-02 11:13:46 +0100

    day1 part1

---
+++ 2024/Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using AocShared;
+
+var fr = new FileReader();
+var list1 = new List<int>();
+var list2 = new List<int>();
+foreach (var line in fr.Lines) {
+    var splitted = line.Split("   "); 
+    list1.Add(int.Parse(splitted[0].Trim()));
+    list2.Add(int.Parse(splitted[1].Trim()));
+}
+
+list1.Sort();
+list2.Sort();
+var sum = 0;
+for (int i = 0; i < list1.Count; i++) {
+    sum+=Math.Abs(list1[i] - list2[i]);
+}
+Console.WriteLine(sum);
---
+++ AocShared/FileReader.cs
@@ -0,0 +1,114 @@
+﻿namespace AocShared;
+
+/// <summary>
+/// Liest ein txt. File ein.
+/// </summary>
+public class FileReader
+{
+    public const string RetoPath = "./Reto/" + FilePath;
+    public const string DavidePath = "./Davide/" + FilePath;
+
+    public string[] Lines;
+
+    private const string FilePath = "./data/data.txt";
+    private char[,] Matrix;
+
+
+    public FileReader(string path = FilePath)
+    {
+        Console.Write(path);
+        if (!string.IsNullOrEmpty(path)){
+            ReadLines(path);
+        }
+    }
+
+    private void ReadLines(string path)
+    {
+        // Pfad zur Datei, die Sie einlesen möchten
+        string[] lines = Array.Empty<string>();
+
+        try
+        {
+            // Alle Zeilen aus der Datei lesen und in einem Array speichern
+            Lines = File.ReadAllLines(path);
+        }
+        catch (IOException e)
+        {
+            // Fehlerbehandlung für den Fall, dass die Datei nicht gelesen werden kann
+            Console.WriteLine("Fehler beim Lesen der Datei: " + e.Message);
+        }
+    }
+
+    public void PrintLines(){
+        foreach (var item in Lines)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public char[,] GetMatrix()
+    {
+        Matrix = new char[Lines.Length, Lines[0].Length];
+
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            for (int j = 0; j < Lines[i].Length; j++)
+            {
+                Matrix[i, j] = Lines[i][j];
+            }
+        }
+        return Matrix;
+    }
+
+    public Dictionary<(int x,int y),char> GetMap(){
+        var dict = new Dictionary<(int,int),char>();
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            for (int j = 0; j < Lines[i].Length; j++)
+            {
+                dict.Add((i, j), Lines[i][j]);
+            }
+        }
+        return dict;
+    }
+
+    public Tuple<int, int>? GetKoordinates(char c)
+    {
+        for (int i = 0; i < Matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < Matrix.GetLength(1); j++)
+            {
+                if (Matrix[i, j] == c)
+                {
+                    return Tuple.Create(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    public string[] VerticalLines()
+    {
+        string[] verticalLines = new string[Lines[0].Length];
+        for (int i = 0; i < verticalLines.Length; i++)
+        {
+            foreach (var line in Lines)
+            {
+                verticalLines[i] += line[i];
+            }
+
+        }
+        return verticalLines;
+    }
+    public List<List<char>> CharLists()
+    {
+        List<List<char>> charLists = new();
+
+        foreach (var line in Lines)
+        {
+            charLists.Add(line.ToList());
+        }
+
+        return charLists;
+    }
+}
